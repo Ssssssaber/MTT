afb17c788cc9e57edf6c6e2c64c1f5882ecdb5f4
Ssssssaber <bladimir.lobanov@gmail.com>
2025-09-02 15:43:32 +0700

    (udpate): restart the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,20 +8,27 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private Vector3 _gravity = new Vector3(0.0f, -9.81f, 0.0f);
     [SerializeField]
+    private CameraFollow _cameraFollow;
+
+    [SerializeField]
     private GameObject _miniCubePrefab;
     [SerializeField]
-    private GameObject _playerCube;
+    private GameObject _playerCubePrefab;
 
     [SerializeField]
     private GameObject _box;
+
     [SerializeField]
     private GameObject _plane;
 
+    private bool _gameStarted = false;
+    private GameObject _objectsParent;
+    private CubeBehaviour _currentPlayerCube;
     private void SpawnMiniCube(Vector3 position)
     {
-        var cube = Instantiate(_miniCubePrefab, _box.transform);
+        var cube = Instantiate(_miniCubePrefab, _objectsParent.transform);
         cube.transform.localPosition = position;
-        cube.GetComponent<Attracted>().SetAttractedTo(_playerCube);
+        cube.GetComponent<Attracted>().SetAttractedTo(_currentPlayerCube.gameObject);
     }
 
     private void FillPlaneWithCubes(Vector3 offset, int width, int height, float spacing)
@@ -44,11 +51,28 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        RestartTheGame(); 
+    }
+
+    public void RestartTheGame(uint cubeCount = 100)
+    {
+        if (_gameStarted)
+        {
+            Destroy(_objectsParent);
+        }
+
         MainLogger.Info("Game Started");
+
+        _objectsParent = Instantiate(new GameObject("ObjectsParent"), _box.transform);
+
+        _currentPlayerCube = Instantiate(_playerCubePrefab, _objectsParent.transform).GetComponent<CubeBehaviour>();
+        _cameraFollow.SetTarget(_currentPlayerCube.transform);
 
         Vector3 _planeSize = _plane.GetComponent<Renderer>().bounds.size / 2;
         Vector3 offset = new Vector3(-_planeSize.x / 2, 0, -_planeSize.z / 2);
         FillPlaneWithCubes(_plane.transform.localPosition + offset, (int)(_planeSize.x / 2), (int)(_planeSize.z / 2), 2f);
+
+        _gameStarted = true;
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerCube/CameraFollow.cs
+++ Assets/Scripts/PlayerCube/CameraFollow.cs
@@ -33,6 +33,6 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        FollowTarget();
+        if (_target) FollowTarget();
     }
 }
